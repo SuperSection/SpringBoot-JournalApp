5c241d9652e51619327908fce52a94ccf342f445
SuperSection <soumosarkar297@gmail.com>
2024-07-03 22:37:49 +0530

    set Profiles in Spring Boot

--- src/main/java/com/supersection/journalApp/JournalApplication.java
+++ src/main/java/com/supersection/journalApp/JournalApplication.java
@@ -2,12 +2,16 @@ package com.supersection.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class JournalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JournalApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(JournalApplication.class, args);
+		System.out.println("Active Profiles: " + Arrays.toString(context.getEnvironment().getActiveProfiles()));
 	}
 
 }
